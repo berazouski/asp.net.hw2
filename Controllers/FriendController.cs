6d24b93d31dbf318181165e5d10a1c1fe8ca8fdd
Viktar Berazouski <berezovskij.work@gmail.com>
2022-11-27 21:05:38 +0300

    Signed-off-by: Viktar Berazouski <berezovskij.work@gmail.com>

---
+++ Controllers/FriendController.cs
@@ -0,0 +1,50 @@
+﻿using HomeWork_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork_2.Controllers
+{
+    public class FriendController : Controller
+    {
+        List<Friend>  Friends;
+        
+        String StateObject = "";
+        
+        public IActionResult Index()
+        {
+            Friends = new List<Friend>
+            {
+                new Friend() { FriendId = 1, FriendName = "User 1", Place = "close friend"},
+                new Friend() { FriendId = 1, FriendName = "User 2", Place = "close friend"},
+                new Friend() { FriendId = 1, FriendName = "User 3", Place = "Making friend"},
+
+            };
+            
+            return View(Friends);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       
+        public IActionResult Edit(Friend friend)
+        {
+            return View(friend);
+        }
+
+        [HttpPost]
+        public string SaveChangeFriend(Friend friend)
+        {
+            //change item in bd
+            StateObject = "Object was has saved";
+            return StateObject;
+        }
+        [HttpGet]
+        public IActionResult Delete(Friend friend)
+        {
+            return View(friend);
+        }
+        
+    }
+}
---
+++ Models/Friend.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeWork_2.Models
+{
+    public class Friend
+    {
+        public int? FriendId { get; set; }
+        public string? FriendName { get; set; }
+
+        public string Place { get; set; }
+
+
+    }
+}
